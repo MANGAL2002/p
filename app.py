dbf31307ad08e72f0036b2e299a96b3ad904d6ac
Ayushi Mangal <83300038+MANGAL2002@users.noreply.github.com>
2025-10-29 14:17:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,86 +1,75 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import pymysql
-app = Flask(__name__)
+from flask_cors import CORS
+import os
 
+app = Flask(__name__)
+CORS(app)
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_USER = os.getenv("DB_USER", "root")
+DB_PASSWORD = os.getenv("DB_PASSWORD", "")
+DB_NAME = os.getenv("DB_NAME", "subjects")
+
+import os
+import pymysql
 
 def get_database_connection():
-    return pymysql.connect(host="localhost", user="root", password="", database="subjects")
+    return pymysql.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME"),
+        port=3306,
+        cursorclass=pymysql.cursors.DictCursor
+    )
 
-@app.after_request
-def add_cors_headers(response):
-    response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "GET, POST, PUT, DELETE, OPTIONS"
-    response.headers["Access-Control-Allow-Headers"] = "Content-Type"
-    return response
-
-@app.route("/", methods=["GET"])
-def get_all_scp_data():
+@app.route('/')
+def get_all_scp():
     conn = get_database_connection()
     cur = conn.cursor()
-    cur.execute("SELECT * FROM sub")
-    rows = cur.fetchall()
+    cur.execute("SELECT * FROM scp_subjects")
+    data = cur.fetchall()
+    cur.close()
     conn.close()
+    return jsonify({"scp data": data})
 
-    data = []
-    for row in rows:
-        data.append({
-            "id": row[0],
-            "item": row[1],
-            "class": row[2],
-            "description": row[3],
-            "containment": row[4]
-        })
-    return {"scp data": data}
-
-@app.route("/scp/<int:id>", methods=["GET"])
-def get_scp_by_id(id):
-    conn = get_database_connection()
-    cur = conn.cursor()
-    cur.execute("SELECT * FROM sub WHERE id=%s", (id,))
-    row = cur.fetchone()
-    conn.close()
-
-    if row:
-        return {"scp data": {
-            "id": row[0],
-            "item": row[1],
-            "class": row[2],
-            "description": row[3],
-            "containment": row[4]
-        }}
-    else:
-        return "SCP ID not found"
-
-@app.route("/scp", methods=["POST"])
+@app.route('/scp', methods=['POST'])
 def add_scp():
-    data = request.get_json()
+    new_data = request.get_json()
     conn = get_database_connection()
     cur = conn.cursor()
-    cur.execute("INSERT INTO sub (item, class, description, containment) VALUES (%s, %s, %s, %s)",
-                (data["item"], data["class"], data["description"], data["containment"]))
+    cur.execute(
+        "INSERT INTO scp_subjects (item, class, description, containment) VALUES (%s, %s, %s, %s)",
+        (new_data['item'], new_data['class'], new_data['description'], new_data['containment'])
+    )
     conn.commit()
+    cur.close()
     conn.close()
-    return "SCP added successfully"
+    return jsonify({"message": "SCP added successfully"})
 
-@app.route("/scp/<int:id>", methods=["PUT"])
+@app.route('/scp/<int:id>', methods=['PUT'])
 def update_scp(id):
-    data = request.get_json()
+    update_data = request.get_json()
     conn = get_database_connection()
     cur = conn.cursor()
-    cur.execute("UPDATE sub SET item=%s, class=%s, description=%s, containment=%s WHERE id=%s",
-                (data["item"], data["class"], data["description"], data["containment"], id))
+    cur.execute(
+        "UPDATE scp_subjects SET item=%s, class=%s, description=%s, containment=%s WHERE id=%s",
+        (update_data['item'], update_data['class'], update_data['description'], update_data['containment'], id)
+    )
     conn.commit()
+    cur.close()
     conn.close()
-    return "SCP updated successfully"
+    return jsonify({"message": "SCP updated successfully"})
 
-@app.route("/scp/<int:id>", methods=["DELETE"])
+@app.route('/scp/<int:id>', methods=['DELETE'])
 def delete_scp(id):
     conn = get_database_connection()
     cur = conn.cursor()
-    cur.execute("DELETE FROM sub WHERE id=%s", (id,))
+    cur.execute("DELETE FROM scp_subjects WHERE id=%s", (id,))
     conn.commit()
+    cur.close()
     conn.close()
-    return "SCP deleted successfully"
+    return jsonify({"message": "SCP deleted successfully"})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000)
