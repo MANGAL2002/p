96e4086b782135633605de6512505160d906e7dd
Ayushi Mangal <83300038+MANGAL2002@users.noreply.github.com>
2025-10-29 14:40:56 +0530

    Update app.py

--- app.py
+++ app.py
@@ -73,4 +73,4 @@ def delete_scp(id):
     return jsonify({"message": "SCP deleted successfully"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000,debug=True)
